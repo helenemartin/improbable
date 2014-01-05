f1e21b079aece3b74104cb24e34ea80e18ffce68
Helene Martin <helenemartin13@hotmail.com>
2014-01-05 22:13:53 +0000

    apply javascript play/pause button on both video clips

--- js/main.js
+++ js/main.js
@@ -8,3 +8,14 @@ $(function() {
     }
   });
 });
+
+$(function() {
+  $(".playpause").click(function(){
+    var video= $("#graph")[0];
+    if (video.paused) {
+      video.play();
+    } else {
+      video.pause();
+    }
+  });
+});
