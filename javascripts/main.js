2af1c701901bb40bbb506e10bc10f5820893d834
Helene Martin <helenemartin13@hotmail.com>
2014-01-08 21:12:44 +0000

    change javascript for playpause button

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,8 +3,10 @@ $(function() {
     var video= $("#girl")[0];
     if (video.paused) {
       video.play();
+      $(".playpause img").attr('src', 'img/animedium.gif');
     } else {
       video.pause();
+      $(".playpause img").attr('src', 'img/still.gif');
     }
   });
 });
