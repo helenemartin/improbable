e0dc642fb36de7c8791fbfd505c36c3c7b2bba90
Helene Martin <helenemartin13@hotmail.com>
2014-01-09 18:21:31 +0000

    add girl.mp4 to index with z index modified javascript to play 3 videos simultaneously

--- js/main.js
+++ js/main.js
@@ -24,13 +24,16 @@ $(function() {
   $(".playpause").click(function(){
     var video= $("#girl")[0];
     var video2= $("#graph")[0];
+    var video3= $("#girl3")[0];
     if (video.paused) {
       video.play();
       video2.play();
+      video3.play();
       $(".playpause img").attr('src', 'img/animedium.gif');
     } else {
       video.pause();
       video2.pause();
+      video3.pause();
       $(".playpause img").attr('src', 'img/still.gif');
     }
   });
